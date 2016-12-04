637dd17392a69b6f204edc55225f1556eae12266
Raquell Montalban <rmch.969@gmail.com>
2016-12-04 15:15:17 -0400

    2do commit

--- app/src/main/java/raquell/mont/tareas/MainActivity.java
+++ app/src/main/java/raquell/mont/tareas/MainActivity.java
@@ -2,6 +2,8 @@ package raquell.mont.tareas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        EditText txt=(EditText)findViewById(R.id.editText2);
+        txt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                        v.setBackgroundResource(R.drawable.estilo_borde_texto);
+                    }
+
+                }
+            });
+        }
 }
